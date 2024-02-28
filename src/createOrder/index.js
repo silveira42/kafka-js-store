3ca00d45f86c116609608e294ffc2d86fcba45c8
Bruno Silveira <silverdev42@gmail.com>
2024-02-28 20:41:11 -0300

    Added try catch to api fetching

--- src/createOrder/index.js
+++ src/createOrder/index.js
@@ -46,13 +46,17 @@ const sendRequest = async (body, url, method) => {
 		};
 	}
 
-	const response = await fetch(url, options)
+	try {
+		const response = await fetch(url, options)
 
-	const apiResponse = await response.json()
+		const apiResponse = await response.json()
 
-  if (response.status < 200 || response.status >= 300) {
-		return `Error: ${apiResponse.message}`
-  } else {
-    return `Success! Order Id: ${apiResponse.orderId}`;
-  }
+		if (response.status < 200 || response.status >= 300) {
+			return `Error: ${apiResponse.message}`
+		} else {
+			return `Success! Order Id: ${apiResponse.orderId}`;
+		}
+	} catch (exception) {
+		return `Error: ${exception.message}`
+	}
 }
